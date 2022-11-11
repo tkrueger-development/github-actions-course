7409b678a6a3603ebc980de4139d7e5e0e097657
Tim Krueger <timkrueger.development@gmail.com>
2022-11-11 01:27:07 +0100

    initial commit

---
+++ src/app.spec.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+
+import { app } from './app';
+
+describe('App test', () => {
+  it('should be defined', () => {
+    expect(app).toBeDefined();
+  });
+});
